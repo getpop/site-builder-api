3d0b45a0d7d85b8947168138c809631f6f5b4eb6
leoloso <leo@getpop.org>
2021-07-02 11:35:09 +0000

    Merge pull request #761 from leoloso/add-sitebuilder-wassup-again
    Downgrade SiteBuilder and Wassup layers again

---
+++ src/Helpers/APIUtils.php
@@ -0,0 +1,41 @@
+<?php
+namespace PoP\SiteBuilderAPI\Helpers;
+
+use PoP\ComponentModel\Misc\GeneralUtils;
+use PoP\ComponentModel\State\ApplicationState;
+use PoP\Definitions\Configuration\Request;
+use PoP\API\Response\Schemes as APISchemes;
+
+class APIUtils
+{
+    public static function getEndpoint(string $url, ?array $dataoutputitems = null): string
+    {
+        $dataoutputitems = $dataoutputitems ?? [
+            \PoP\ComponentModel\Constants\DataOutputItems::MODULE_DATA,
+            \PoP\ComponentModel\Constants\DataOutputItems::DATABASES,
+            \PoP\ComponentModel\Constants\DataOutputItems::DATASET_MODULE_SETTINGS,
+        ];
+        $endpoint = GeneralUtils::addQueryArgs([
+            \PoP\ComponentModel\Constants\Params::SCHEME => APISchemes::API,
+            \PoP\ComponentModel\Constants\Params::OUTPUT => \PoP\ComponentModel\Constants\Outputs::JSON,
+            \PoP\ComponentModel\Constants\Params::DATAOUTPUTMODE => \PoP\ComponentModel\Constants\DataOutputModes::COMBINED,
+            // \PoP\ComponentModel\Constants\Params::DATABASESOUTPUTMODE => \PoP\ComponentModel\Constants\DatabasesOutputModes::COMBINED,
+            \PoP\ComponentModel\Constants\Params::DATA_OUTPUT_ITEMS => implode(
+                \PoP\ComponentModel\Tokens\Param::VALUE_SEPARATOR,
+                $dataoutputitems
+            ),
+        ], $url);
+
+        $vars = ApplicationState::getVars();
+        if ($mangled = $vars['mangled']) {
+            $endpoint = GeneralUtils::addQueryArgs(
+                [
+                    Request::URLPARAM_MANGLED => $mangled,
+                ],
+                $endpoint
+            );
+        }
+
+        return $endpoint;
+    }
+}
